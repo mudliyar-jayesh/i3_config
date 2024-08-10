4b2829da0e44922eb7aeef1d10660d9ffa7fb75c
Mudliyar Jayesh <mudliyar.jayesh@gmail.com>
2024-08-10 09:30:19 +0530

    clangd added lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -94,3 +94,21 @@ lspConfig.rust_analyzer.setup{
         }
     }
 }
+
+
+lspConfig.clangd.setup{
+    cmd = {"clangd"},
+    filetypes = {"c", "cpp", "objc", "objcpp", "cuda", "proto"},
+    root_dir = lspConfig.util.root_pattern(
+          '.clangd',
+          '.clang-tidy',
+          '.clang-format',
+          'compile_commands.json',
+          'compile_flags.txt',
+          'configure.ac',
+          '.git'
+        ),
+    single_file_support = true,
+      
+
+}
